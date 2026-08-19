9e4575f68b74e0a61ae64ad5e104ed13fe521e3d
Emilson de Souza Nascimento <emilsonsn@users.noreply.github.com>
2026-08-19 08:32:18 -0300

    fix: cors

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,15 @@
 <?php
 
 return [
-    'paths' => ['api/*'],
+    'paths' => [
+        'api/*',
+        'sanctum/csrf-cookie',
+    ],
     'allowed_methods' => ['*'],
     'allowed_origins' => array_filter(explode(',', env('FRONTEND_URL', 'http://localhost:4200'))),
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 ];
